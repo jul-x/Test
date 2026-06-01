2f829fff11594ebed2f8baf88a6982e67c15bb8c
jul-x <greatfreya13@gmail.com>
2026-06-01 05:50:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-const data = {
+const originalData = {
     A: ["My Biggest Fear", "My Favorite Film", "My Favorite School Subject", "My Favorite Invention", "My Perfect Weekend"],
     B: ["Past Simple", "Present Perfect", "Used to", "Passive Voice"]
 };
+let currentData = { A: [...originalData.A], B: [...originalData.B] };
 
 function drawCard(type) {
-    const list = data[type];
-    if (list.length === 0) {
-        document.getElementById('res' + type).innerText = "Карты закончились!";
-        return;
-    }
+    const list = currentData[type];
+    if (list.length === 0) return;
+
+    const deckElement = document.getElementById('deck' + type);
+    const resElement = document.getElementById('res' + type);
+    
+    // Берем случайную карту
     const randomIndex = Math.floor(Math.random() * list.length);
-    const card = list.splice(randomIndex, 1)[0];
-    document.getElementById('res' + type).innerText = card;
+    resElement.innerText = list.splice(randomIndex, 1)[0];
+    
+    // Переворачиваем
+    deckElement.classList.add('flipped');
+}
+
+function resetDeck(type) {
+    const deckElement = document.getElementById('deck' + type);
+    deckElement.classList.remove('flipped');
+    currentData[type] = [...originalData[type]];
 }
